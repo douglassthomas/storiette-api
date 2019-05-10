97d92052c1884f8cbf3b05f2d4f943066c213969
douglassthomas <dougitu@gmail.com>
2019-05-10 11:40:00 +0700

    nambah cors lagi

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,11 @@ let connection = mysql.createConnection(mysqlOpt);
 // });
 
 // ---- page ----
-router.get('/', function (req, res) {
+router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}, function (req, res) {
   res.json('ok')
 })
 
