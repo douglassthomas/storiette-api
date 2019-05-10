824a7d54f9046261793daa292405efa06f0e4246
douglassthomas <dougitu@gmail.com>
2019-05-10 13:43:17 +0700

    detail

--- routes/index.js
+++ routes/index.js
@@ -127,7 +127,7 @@ router.post('/doLogin',function(req, res, next) {
 
 })
 
-router.get('/detail', function(req, res, next) {
+router.post('/detail', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
